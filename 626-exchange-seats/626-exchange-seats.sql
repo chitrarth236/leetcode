43ede925c898a79162feb6e6dfe887fbfd0861bb
Chitrarth Patel <chitrarth236@gmail.com>
2022-06-09 15:57:46 -0700

    Time: 334 ms (40.97%), Space: 0B (100.00%) - LeetHub

---
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+WITH cte1 AS(SELECT id as x,
+            CASE 
+            WHEN id = (SELECT count(1) from seat) and id%2 != 0 THEN student
+            WHEN id % 2 != 0 THEN (select student from seat as t1 where t1.id=x+1)
+            WHEN id % 2 = 0 THEN (select student from seat as t1 where t1.id=x-1)
+            else (select student from seat as t1 where t1.id=x)
+            end AS student
+            from seat)
+
+SELECT x as id, student FROM cte1;
