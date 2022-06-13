08513f652191a4bd81ab9c38a99495a4c6657aff
Chitrarth Patel <chitrarth236@gmail.com>
2022-06-12 17:48:49 -0700

    Time: 324 ms (46.39%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -5,7 +5,7 @@ WITH cte1 AS(SELECT id as x,
             WHEN id = (SELECT count(1) from seat) and id%2 != 0 THEN student
             WHEN id % 2 != 0 THEN (select student from seat as t1 where t1.id=x+1)
             WHEN id % 2 = 0 THEN (select student from seat as t1 where t1.id=x-1)
-            else (select student from seat as t1 where t1.id=x)
+            #else (select student from seat as t1 where t1.id=x)
             end AS student
             from seat)
 
