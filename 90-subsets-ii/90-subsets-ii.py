98af06e1bfcadbcd7b25c2fb3ad3856f4e187b3d
Chitrarth Patel <chitrarth236@gmail.com>
2022-07-23 23:56:38 -0700

    Time: 45 ms (81.00%), Space: 14.2 MB (9.47%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -3,15 +3,17 @@ class Solution:
         result = []
         processed = []
         nums.sort()
-        self.helper(processed,nums, result)
+        self.helper(processed,nums, 0, result,True)
         return result
         
-    def helper(self, p, up, result):
-        if len(up) == 0:
-            if p not in result:
-                result.append(p)
+    def helper(self, p, up, i, result, isPicked):
+        if i >= len(up):
+            result.append(p)
             return
         cp = p.copy()
-        cp.append(up[0])
-        self.helper(cp, up[1:], result)
-        self.helper(p, up[1:], result)
+        cp.append(up[i])
+        
+        if i == 0 or up[i] != up[i - 1] or isPicked:
+            #i = i + 1
+            self.helper(cp, up, i+1, result,True)
+        self.helper(p, up, i+1, result, False)
